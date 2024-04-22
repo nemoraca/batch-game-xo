f19d24d02fd1611bcb0f404eac0489973761344b
Nenad Moraca <nemoraca@gmail.com>
2024-04-22 18:37:06 +0200

    Second version.

--- GameXO.bat
+++ GameXO.bat
@@ -5,17 +5,18 @@ chcp 437>NUL
 
 setlocal ENABLEDELAYEDEXPANSION
 
-rem We end each line with & rem to ensure there are no additional invisible spaces.
-set a11= & set a12= & set a13= & rem
-set a21= & set a22= & set a23= & rem
-set a31= & set a32= & set a33= & rem
-
 rem We wrap the assignments in quotes to ensure there are no additional invisible spaces.
 set "xo=*xo" & set "ox=*ox"
 set "player1=1st player"
 set "player2=2nd player"
 set "otherplayer1=%player2%"
 set "otherplayer2=%player1%"
+
+:repeat
+rem We end each line with & rem to ensure that there are no additional invisible spaces.
+set a11= & set a12= & set a13= & rem
+set a21= & set a22= & set a23= & rem
+set a31= & set a32= & set a33= & rem
 
 set /a brojac=0
 
@@ -25,7 +26,7 @@ for /l %%i in (1,1,2) do (
 	rem The following batch script code is best viewed in Notepad++ under UTF-8 encoding.
 	rem We draw the playing table using graphical extended ASCII characters from Code Page 850.
 	rem We separate graphical ASCII characters with the escape character (caret) to prevent
-	rem certain graphical symbols (according to UTF-8) from being displayed in Notepad++.
+	rem certain symbols (according to UTF-8) from being displayed in Notepad++.
 	cls
 	rem ========================================================================================
 	echo=
@@ -51,57 +52,75 @@ for /l %%i in (1,1,2) do (
 	echo=   À^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^
 Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^
 Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ù
+
 	echo=
+
+	echo=   Ú^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^
+Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^
+Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^¿
+	echo=   ³                                                             ³
+	echo=   ³                                                             ³
+	echo=   ³                                                             ³
+	echo=   ³                                                             ³
+	echo=   ³                                                             ³
+	echo=   ³                                                             ³
+	echo=   ³                                                             ³
+	echo=   ³                                                             ³
+	echo=   ³                                                             ³
+	echo=   À^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^
+Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^
+Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ä^Ù^A
+	rem A is ANSI Escape Sequence that moves cursor up.
+	echo=AAAAAAAAA
 	rem ========================================================================================
 
 	for /l %%l in (1,1,3) do (
 		if "!a%%l1!!a%%l2!!a%%l3!"=="!ox:~%%i,1!!ox:~%%i,1!!ox:~%%i,1!" (
-			echo=   !otherplayer%%i! won^^!
+			echo=   ³    !otherplayer%%i! won^^!
 			goto end
 			)
 		if "!a1%%l!!a2%%l!!a3%%l!"=="!ox:~%%i,1!!ox:~%%i,1!!ox:~%%i,1!" (
-			echo=   !otherplayer%%i! won^^!
+			echo=   ³    !otherplayer%%i! won^^!
 			goto end
 			)
 		)
 	if "!a11!!a22!!a33!"=="!ox:~%%i,1!!ox:~%%i,1!!ox:~%%i,1!" (
-		echo=   !otherplayer%%i! won^^!
+		echo=   ³    !otherplayer%%i! won^^!
 		goto end
 		)
 	if "!a13!!a22!!a31!"=="!ox:~%%i,1!!ox:~%%i,1!!ox:~%%i,1!" (
-		echo=   !otherplayer%%i! won^^!
+		echo=   ³    !otherplayer%%i! won^^!
 		goto end
 		)
 	rem When delayed expansion is enabled, ECHO ! does not work. ECHO ^^! should be used instead.
-	if !brojac!==10 echo=   Draw^^! & goto end
+	if !brojac!==10 echo=   ³    Draw^^! & goto end
 
 	rem We have to use escape sequences ^( and ^) because we are in a FOR loop block.
-	echo=   !player%%i!: ^(X,Y^)
+	echo=   ³    !player%%i!: ^(X,Y^)
 	rem This is a dummy line, to prevent the occasional odd blinking of the output of the next line.
 	set /p =""<NUL
 	rem We start the strings with BS control character to prevent next 3 spaces from being ignored.
-	set /p x="   X = "
+	set /p x="   ³    X = "
 	set /p =""<NUL
-	set /p y="   Y = "
+	set /p y="   ³    Y = "
 	for /l %%j in (!x!,1,!x!) do (
 		for /l %%k in (!y!,1,!y!) do (
 			if "!a%%j%%k!"==" " (set a%%j%%k=!xo:~%%i,1!) else (
 				echo=
 				rem The sound of bell indicates error.
-				echo=   Error^^!
+				echo=   ³    Error^^!
 				goto end
 				)
 			)
 		)
 	)
 goto loop
+
 :end
 echo=
-rem This is a dummy line, to prevent the occasional odd blinking of the output of the next line.
-set /p =""<NUL
-rem We do not use ECHO because it moves cursor to the next line.
 rem We start the string with BS control character to prevent next 3 spaces from being ignored.
-set /p ="   Press any key to end the game..."<NUL
-rem We redirect output to NUL device to prevent message "Press any key to continue..." from being displayed.
-pause>NUL
+set /p answer="   ³    Do you want to play another game (Y/N)? "
+rem The following condition emulates logical disjunction.
+if "%answer:Y=y%"=="y" goto repeat
+echo=
 echo=
